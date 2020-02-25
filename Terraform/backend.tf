039b9f6fcdb2b38971643d7cb0b858b310868fa7
yayh <yayh@hotmail.com>
2020-02-26 01:33:38 +0400

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 # When authenticating using the Azure CLI or a Service Principal:
 terraform {
   backend "azurerm" {
-    storage_account_name = "tfstacc20042018"
+    storage_account_name = "bgmeadevopsstr"
     container_name       = "terraform"
     key                  = "dev.terraform.tfstate"
   }
