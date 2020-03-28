d7001f90c7933b07bd5371f0ed3b33964b7a1390
yayh <yayh@hotmail.com>
2020-03-28 14:32:42 +0400

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,3 +1,10 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you are using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
 # When authenticating using the Azure CLI or a Service Principal:
 terraform {
   backend "azurerm" {
@@ -5,7 +12,5 @@ terraform {
     container_name       = "terraform"
     key                  = "dev.terraform.tfstate"
   }
-  
-  features {}
 }
 
